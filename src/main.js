762c1ddb0148a229144df84d9518bd441f07c10b
Mansi S <mansisaini26@gmail.com>
2023-05-23 15:57:00 -0700

    create phaser game

---
+++ src/main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    render: {
+        pixelArt: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+
+    scene: [Play],
+}
+
+let game = new Phaser.Game(config) // create phaser game
+
+// define the keys
+let KeyF, keyR, KeyLEFT, keyRIGHT, keyUP;
+
+// border UI size
+let borderUISize = game.config.height / 15; //set UI height
+let borderPadding = borderUISize / 3; // set padding around the game frame
