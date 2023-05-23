276aa5cd0eadccc44722078d70e0a35a905bf17b
Mansi S <mansisaini26@gmail.com>
2023-05-23 15:57:19 -0700

    init play scene

---
+++ src/scenes/Play.js
@@ -0,0 +1,20 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene');
+    }
+
+    // preload assets
+    preload() {
+        
+    }
+
+    // create background and game elements
+    create() {
+
+    }
+
+    // updates every frame
+    update() {
+        
+    }
+}
