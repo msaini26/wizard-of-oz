0482d407f56b78d81bf1f4b186cffff00ff52953
Rebecca Zhao <rzhao29@ucsc.edu>
2023-06-15 07:51:29 -0700

    controls comments done

--- src/scenes/Controls.js
+++ src/scenes/Controls.js
@@ -4,11 +4,12 @@ class Controls extends Phaser.Scene {
     }
 
     preload(){
+        //loading controls background
         this.load.image('controls_background', './assets/background/Gray.png');
     }
 
     create(){
-        // display title image
+        // display controls background image
         this.controls_background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'controls_background').setScale(2).setOrigin(0, 0); // place background tile sprite
 
 
@@ -22,9 +23,11 @@ class Controls extends Phaser.Scene {
                 bottom: 5,
             },
         }
+
+        //adding title text
         this.add.text(game.config.width/2, 75, 'Rules', titleConfig).setOrigin(0.5);
 
-        //title text configuration
+        //sub text configuration
         let subConfig = {
             fontFamily: 'joystix',
             fontSize: '20px',
@@ -36,7 +39,7 @@ class Controls extends Phaser.Scene {
             },
         }
 
-         //title text configuration
+         //text configuration
          let textConfig = {
             fontFamily: 'joystix',
             fontSize: '20px',
@@ -50,12 +53,15 @@ class Controls extends Phaser.Scene {
                 bottom: 5,
             },
         }
+        //text for controls of game
         this.add.text(50, 150, 'Controls:', subConfig);
         this.add.text(210, 150, 'Use LEFT and RIGHT arrow keys to move. UP arrow to jump (you can jump up to 3 times in a row).', textConfig);
 
+        //text for goals
         this.add.text(110, 250, 'Goal:', subConfig);
         this.add.text(210, 250, 'Travel through the Land of Oz and defeat the Evil Witch through 3 levels.', textConfig);
 
+        //text for guidance on first level and what you are playing as
         this.add.text(50, 350, 'Before you begin:', subConfig);
         textConfig.wordWrap = { width: game.config.width - 150 };
         this.add.text(100, 375, 'You are playing as Dorothy who lives in Kansas. All of a sudden, a tornado occurs!', textConfig);
@@ -63,13 +69,13 @@ class Controls extends Phaser.Scene {
         textConfig.color = '#de3731';
         this.add.text(100, 450, 'Try and escape the tornado by jumping on different platforms and last for at least 30 seconds.', textConfig);
         
+        //adding button to click on to play
         titleConfig.color = '#8756cc';
         titleConfig.fontSize = '40px';
-        //adding play text
         this.begin = this.add.text(game.config.width - 100, game.config.height - 40, 'PLAY', titleConfig).setOrigin(0.5);
         this.begin.setInteractive(); //set interactive so mouse click works
 
-        //boolean to keep track that sound effect plays once
+        //boolean to keep track that sound effect plays once and button clicked once
         this.clicked = false;
     }
 
@@ -88,11 +94,10 @@ class Controls extends Phaser.Scene {
         this.begin.on('pointerdown', () => {
             //if sound effect hasn't played yet
             if(!this.clicked){
-                // this.sound.play('chimes'); //play sound
                 this.sound.play('sfx_select'); // play selector sound
                 this.clicked = true; //set clicked to true
             }
-            introMusic.stop(); //stop playing sound
+            introMusic.stop(); //stop playing music
 
             //move to next scene
             this.scene.start('tornadoScene');
